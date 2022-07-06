b0ff6a350cff414506e85ae934f3a5c9b77ff338
Conor White <conorwhite667@gmail.com>
2022-07-06 15:31:44 +0100

    changed println to hello world

---
+++ src/com/qa/project_project_structure/Runner.java
@@ -0,0 +1,8 @@
+package com.qa.project_project_structure; // Must specify class location, what packet it is in
+										  //it's fully qualified package name
+
+public class Runner {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
